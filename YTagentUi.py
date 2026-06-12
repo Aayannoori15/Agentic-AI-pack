16674a237253d80dd9bead1171831eb8b5308aaa
Aayannoori15 <Aayan.noori15@gmail.com>
2026-06-12 11:23:25 -0400

    Changes in streamlit UI

--- YTagentUi.py
+++ YTagentUi.py
@@ -5,7 +5,6 @@ from ytagent import *
 st.set_page_config(
     page_title='youtube video analyser',
 )
-st.write('welcome to youtube video analyser')
 
 st.title('Youtube video analyser')
 
@@ -13,11 +12,11 @@ st.title('Youtube video analyser')
 def get_agent():
     return build_agent()
 agent=get_agent()
-video=st.text_input('enter yt url')
-button=st.button('analyse video')
+video=st.text_input('Enter Youtube URL')
+button=st.button('Analyse video')
 
 if video and button:
-    with st.spinner('analysing video.....'):
+    with st.spinner('Analysing video.....'):
         res=agent.run(
             f'analyse the video {video}'
         )
